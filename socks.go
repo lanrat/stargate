3ac86e4223dc9ad85cd490edb6c2b7da7333eef8
Ian Foster <ian@vorsk.com>
2020-07-09 09:57:56 -0700

    prep for ipv6 support

--- socks.go
+++ socks.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"math/rand"
 	"net"
 
 	"github.com/haxii/socks5"
@@ -12,6 +13,20 @@ func runProxy(proxyAddr *net.TCPAddr, listenAddr string) error {
 	conf.Logger = l
 	d := net.Dialer{LocalAddr: proxyAddr}
 	conf.Dial = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return d.DialContext(ctx, network, addr)
+	}
+	server, err := socks5.New(conf)
+	if err != nil {
+		return err
+	}
+	return server.ListenAndServe("tcp", listenAddr)
+}
+
+func runRandomProxy(proxyAddresses []*net.TCPAddr, listenAddr string) error {
+	conf := &socks5.Config{}
+	conf.Logger = l
+	conf.Dial = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		d := net.Dialer{LocalAddr: proxyAddresses[rand.Intn(len(proxyAddresses))]}
 		return d.DialContext(ctx, network, addr)
 	}
 	server, err := socks5.New(conf)
