7f2a09a856a130e8569006450a4f91a120926d6e
Ian Foster <ian@vorsk.com>
2020-07-09 10:17:44 -0700

    option to disable prosy ports

--- README.md
+++ README.md
@@ -1,28 +1,40 @@
 # Stargate
 
-Stargate runs SOCKS proxies on different ports egressing on sequential IPs in the same subnet.
+Stargate runs TCP SOCKS proxies on different ports egressing on sequential IPs in the same subnet.
 This requires the host running stargate to have the subnet routed directly to it.
 
 ## Usage
 
-```sh
+```
 Usage of ./stargate:
   -listen string
         IP to listen on (default "127.0.0.1")
   -port uint
-        first port to start listening on (default 10000)
+        first port to start listening on
   -proxy string
         CIDR notation of proxy IPs (default "127.0.0.1/32")
   -random uint
         port to use for random proxy server
 ```
 
+## Random
+
+The `-random` flag starts a SOCKS5 proxy that egresses traffic on a random IP in the subnet.
+This is useful for avoid rate-limiting or in situations where there are too many IPs in the subnet to listen on each port which is common with IPv6.
+
 ## Example
 
 The following will start 255 SOCKS proxies listening on 127.0.0.7 ports 10000-100256 sending traffic egressing on 12.34.56.0 through 12.34.56.254.
 
-```sh
+```
 ./stargate -listen 127.0.0.7 -port 10000 -proxy 12.34.56.0/24
+```
+
+The following will start a single socks proxy listening on 127.0.0.1:1337 egressing each connection from a random IP in 12.34.56.0/24.
+
+```
+./stargate -random 1337 -proxy 12.34.56.0/24
+
 ```
 
 ### [Docker](https://cloud.docker.com/repository/docker/lanrat/stargate)
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var (
 	l *log.Logger
 
 	listenIP = flag.String("listen", "127.0.0.1", "IP to listen on")
-	port     = flag.Uint("port", 10000, "first port to start listening on")
+	port     = flag.Uint("port", 0, "first port to start listening on")
 	proxy    = flag.String("proxy", "127.0.0.1/32", "CIDR notation of proxy IPs")
 	random   = flag.Uint("random", 0, "port to use for random proxy server")
 )
@@ -24,11 +24,15 @@ func main() {
 	flag.Parse()
 	l = log.New(os.Stderr, "", log.LstdFlags)
 
+	if *port == 0 && *random == 0 {
+		l.Fatal("no SOCKS proxy ports provided, pass -port and/or -random")
+	}
+
 	ips, err := hosts(*proxy)
 	check(err)
 
 	// check that random port is outside range of other proxies
-	if *random != 0 && *random >= *port && int(*random) < (int(*port)+len(ips)) {
+	if *port != 0 && *random != 0 && *random >= *port && int(*random) < (int(*port)+len(ips)) {
 		l.Fatalf("random port %d inside range %d-%d", *random, *port, int(*port)+len(ips))
 	}
 
@@ -37,14 +41,16 @@ func main() {
 	check(err)
 
 	var work errgroup.Group
-	l.Printf("starting on %d IPs\n", len(addresses))
-	for num, address := range addresses {
-		listenPort := num + int(*port)
-		address := address // https://golang.org/doc/faq#closures_and_goroutines
-		work.Go(func() error {
-			l.Printf("Starting proxy %s:%d on IP: %s\n", *listenIP, listenPort, address.IP.String())
-			return runProxy(address, fmt.Sprintf("%s:%d", *listenIP, listenPort))
-		})
+	if *port != 0 {
+		l.Printf("starting on %d IPs\n", len(addresses))
+		for num, address := range addresses {
+			listenPort := num + int(*port)
+			address := address // https://golang.org/doc/faq#closures_and_goroutines
+			work.Go(func() error {
+				l.Printf("Starting proxy %s:%d on IP: %s\n", *listenIP, listenPort, address.IP.String())
+				return runProxy(address, fmt.Sprintf("%s:%d", *listenIP, listenPort))
+			})
+		}
 	}
 
 	// start random proxy if set
