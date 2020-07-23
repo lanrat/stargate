9e2d6acfdc6f6842d6ae72437f373ad894178432
Ian Foster <ian@vorsk.com>
2020-07-23 08:43:19 -0700

    freebsd freebind option

---
+++ freebind_freebsd.go
@@ -0,0 +1,33 @@
+// +build freebsd
+
+package main
+
+import (
+	"fmt"
+	"syscall"
+)
+
+func controlFreebind(network, address string, c syscall.RawConn) error {
+	if err := freeBind(network, address, c); err != nil {
+		return err
+	}
+	return nil
+}
+
+// from https://github.com/zrepl/zrepl/blob/master/util/tcpsock/tcpsock_freebind_freebsd.go
+func freeBind(network, address string, c syscall.RawConn) error {
+	var err, sockerr error
+	err = c.Control(func(fd uintptr) {
+		if network == "tcp6" {
+			sockerr = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_IPV6, syscall.IPV6_BINDANY, 1)
+		} else if network == "tcp4" {
+			sockerr = syscall.SetsockoptInt(int(fd), syscall.IPPROTO_IP, syscall.IP_BINDANY, 1)
+		} else {
+			sockerr = fmt.Errorf("expecting 'tcp6' or 'tcp4', got %q", network)
+		}
+	})
+	if err != nil {
+		return err
+	}
+	return sockerr
+}
