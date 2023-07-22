fe4669d88e0d87a0746a5599c772230612ac7c48
Matthew Bryant (mandatory) <matt@refinery.io>
2023-07-22 12:04:12 -0700

    Added new subnet rotation feature

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/lanrat/stargate
 
 require (
+	github.com/dspinhirne/netaddr-go v0.0.0-20211008142535-a4c5bccad224 // indirect
 	github.com/haxii/socks5 v1.0.0
 	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29
 )
--- socks.go
+++ socks.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"net"
-
+	"math/rand"
+	"time"
 	"github.com/haxii/socks5"
 )
 
@@ -40,6 +41,54 @@ func runRandomProxy(cidr *net.IPNet, listenAddr string) error {
 	}
 	conf.Dial = func(ctx context.Context, network, addr string) (net.Conn, error) {
 		ip := randomIP(cidr)
+		v("random %s proxy (%q) request for: %q", network, ip.String(), addr)
+		d := net.Dialer{
+			LocalAddr: &net.TCPAddr{
+				IP: ip,
+			},
+			Control: controlFreebind,
+		}
+		return d.DialContext(ctx, network, addr)
+	}
+	server, err := socks5.New(conf)
+	if err != nil {
+		return err
+	}
+	return server.ListenAndServe("tcp", listenAddr)
+}
+
+// runRandomSubnetProxy starts a proxy listening on listenAddr that egresses every connection on a subnet
+// in the cidr range defined. It will shuffle the ranges and then evenly distribute the connections so
+// that a subnet is only reused after all the others have been used.
+func runRandomSubnetProxy(listenAddr string, iprange string, newCidr uint) error {
+	subnets, err := breakIntoSubnets(iprange, newCidr)
+    rand.Seed(time.Now().UnixNano())
+    rand.Shuffle(len(subnets), func(i, j int) { subnets[i], subnets[j] = subnets[j], subnets[i] })
+
+	v("we were given network %s with a cidr of %d. our subnet pool size is %d", iprange, newCidr, len(subnets))
+
+	// Keeps track of what subnet we're on in our rotation
+	subnet_pointer := 0
+
+	conf := &socks5.Config{
+		Logger:   l,
+		Resolver: resolver,
+	}
+	conf.Dial = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		current_subnet := subnets[subnet_pointer]
+
+		v("pulling ip from subnet %s (range %d/%d)", current_subnet, subnet_pointer, len(subnets))
+		_, cidr, _ := net.ParseCIDR(current_subnet)
+		ip := randomIP(cidr)
+		subnet_pointer++
+
+        // Reset pointer to wrap around once an IP
+        // from each subnet has been used.
+        if subnet_pointer >= len(subnets) {
+            v("used all the subnets in our pool, looping back around...")
+            subnet_pointer = 0
+        }
+
 		v("random %s proxy (%q) request for: %q", network, ip.String(), addr)
 		d := net.Dialer{
 			LocalAddr: &net.TCPAddr{
